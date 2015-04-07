b756009dba38784781afebe2bafecefb76b0a45d
alex011235 <st.alexander85@gmail.com>
2015-04-07 22:21:03 +0200

    matching

--- matching.py
+++ matching.py
@@ -3,7 +3,6 @@ import cv2
 from matplotlib import pyplot as plt
 
 def matches(img1,img2):
-	MIN_MATCH_COUNT = 10
 	hessian_tresh = 500 # may speed up, but gives smaller amount of kp.
 	surf = cv2.SURF(500,upright=True)
 	# SURF keypoints and descriptors
@@ -16,8 +15,8 @@ def matches(img1,img2):
 	flann = cv2.FlannBasedMatcher(index_params, search_params)
 	matches = flann.knnMatch(des1,des2,k=2)
 
-	# store all the good matches as per Lowe's ratio test.
-	best_m = [] # best_matches
+	# best_matches. 
+	best_m = [] 
 	for a,b in matches:
 	    if a.distance < 0.7*b.distance:
 		best_m.append(a)
@@ -27,10 +26,7 @@ def matches(img1,img2):
 	img2_pts = np.float32([ kp2[m.trainIdx].pt for m in best_m ]).reshape(-1,1,2)
 	return img1_pts, img2_pts
 
-	
-
-
-
+# Test code	
 img1 = cv2.imread('bilds/ny2.jpg',0)
 img2 = cv2.imread('bilds/ny1.jpg',0) 
 img11 = cv2.imread('bilds/ny2.jpg')
@@ -40,10 +36,7 @@ img22 = img22[:, :, ::-1]
 
 img1_pts, img2_pts = matches(img1,img2)
 f, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
-#ax1.plot(x, y)
-#ax1.set_title('Sharing Y axis')
-#ax2.scatter(x, y)
-
+ 
 ax1.imshow(img11)
 for p in img1_pts:      		
 	ax1.plot(p[0][0],p[0][1],'ro')
